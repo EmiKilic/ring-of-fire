26c8a609526bb9a112a30b7503d7b7c8c7d686eb
EmiKilic <emirhan_kilic@outlook.de>
2024-09-11 11:17:27 +0200

    added array with shuffled deck

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -1,23 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GameComponent],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
-
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  game: Game | undefined;
 
-constructor() {
+  constructor() {}
 
-}
+  ngOnInit(): void {
+    this.newGame();
+  }
 
-takeCard() {
-  this.pickCardAnimation = true;
-}
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
 
+  takeCard() {
+    this.pickCardAnimation = true;
+  }
 }
