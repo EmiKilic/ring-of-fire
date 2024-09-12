0e2b796d1c9a3ede33cdeaa6f1e0648e1aefac93
EmiKilic <emirhan_kilic@outlook.de>
2024-09-12 12:24:35 +0200

    add new player function

--- ringoffire/src/models/game.ts
+++ ringoffire/src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-  public players: string[] = ['Hans', 'Maria', 'Tobi', 'Anna'];
+  public players: string[] = [];
   public stack: string[] = [];
   public playedCards: string[] = [];
   public currentPlayer: number = 0;
